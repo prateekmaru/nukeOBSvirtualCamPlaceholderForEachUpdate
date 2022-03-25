a5701b00bbb2330f9c808716cc081f4757076e4c
Prateek Maru <prateekmaru@yahoo.com>
2022-03-25 12:03:43 +0530

    Nuke OBS Virtual Cam PlaceHolder Image xD! .bat File

---
+++ obs_placeholder_nuke.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+ECHO =================================================
+ECHO OBS Virtual Camera Placeholder PNG Nuke!
+ECHO RUN AS ADMIN COZ Program File Directory!!
+ECHO Script By Lazy._.Prateek11
+ECHO =================================================
+cd "C:\Program Files\obs-studio\data\obs-plugins\win-dshow"
+del _placeholder.png
+ren placeholder.png _placeholder.png
+ECHO Done [1.0000000000]   
+PAUSE
